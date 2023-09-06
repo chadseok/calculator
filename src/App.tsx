c5968dfe8443896e2be41c2a82e9f81d1f43addb
changhwan seok <83488329+chadseok@users.noreply.github.com>
2023-09-06 18:21:51 +0900

    App.tsx에 전체 화면 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,33 @@
+import useTheme from "@/libs/hooks/useTheme";
+import useHistory from "./libs/hooks/useHistory";
+import Calculator from "@/components/Calculator";
+import History from "@/components/History";
 import "./App.css";
 
-function App() {
-  return <></>;
-}
+const App = () => {
+  const { theme, toggleTheme } = useTheme();
+  const { history, resetHistory, isHistoryOn, toggleHistoryOn } = useHistory();
+
+  return (
+    <div className="app">
+      <header className="header">
+        <div className="header__logo">🤔 calculator</div>
+        <div className="header__tool">
+          <button className="header__history" onClick={toggleHistoryOn}>
+            📜
+          </button>
+          <button className="header__theme" onClick={toggleTheme}>
+            {theme === "dark" ? "🌜" : "🌞"}
+          </button>
+        </div>
+      </header>
+      {isHistoryOn ? (
+        <History history={history} resetHistory={resetHistory} />
+      ) : (
+        <Calculator />
+      )}
+    </div>
+  );
+};
 
 export default App;
